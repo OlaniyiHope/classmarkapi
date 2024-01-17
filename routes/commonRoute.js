6083cdaedf99605f709af9f0cea45252311d7056
OlaniyiHope <olaniyihoppee@gmail.com>
2024-01-17 11:16:51 +0100

    all

--- routes/commonRoute.js
+++ routes/commonRoute.js
@@ -1,4 +1,3 @@
-// authRoutes.js
 import express from "express";
 import {
   login,
@@ -16,65 +15,73 @@ import {
   updateTeacherById,
   getTeacherById,
 } from "../controller/authController.js";
-import multer from "multer";
-import AWS from "aws-sdk";
-import multerS3 from "multer-s3";
+// commonRoute.js
 
+import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3 } from "@aws-sdk/client-s3";
 import authenticateUser from "../middleware/authMiddleware.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import fs from "fs";
+
 const router = express.Router();
 
-const s3 = new AWS.S3();
+// Modify the commonRoute function to accept the s3 instance
+const commonRoute = (s3) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+  const uploadDir = path.join(__dirname, "..", "uploads");
 
-const uploadDir = path.join(__dirname, "..", "uploads");
+  fs.mkdirSync(uploadDir, { recursive: true });
 
-// Create the 'uploads' directory if it doesn't exist
-fs.mkdirSync(uploadDir, { recursive: true });
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: "edupros",
-    acl: "public-read", // Set the appropriate ACL
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: function (req, file, cb) {
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
       cb(null, `${Date.now()}-${file.originalname}`);
     },
-  }),
-});
-router.post("/register", register);
-router.post("/login", login);
-router.get("/users/:role", getUserByRole);
-router.get("/students/:id", authenticateUser, getStudentById);
-router.get("/teachers/:id", authenticateUser, getTeacherById);
+  });
 
-router.get("/student/:className", getStudentsByClass); // New route for getting students by class
-router.get("/get-admin", authenticateUser, getAdmin);
-router.put("/students/:id", authenticateUser, updateStudentById);
-router.put("/teachers/:id", authenticateUser, updateTeacherById);
+  const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: "edupros", // Replace with your bucket name
+      acl: "public-read",
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      key: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`);
+      },
+    }),
+  });
 
-router.delete("/users/:userId", deleteUser);
-// router.post("/setting", createSetting);
+  router.post("/register", register);
+  router.post("/login", login);
+  router.get("/users/:role", getUserByRole);
+  router.get("/students/:id", authenticateUser, getStudentById);
+  router.get("/teachers/:id", authenticateUser, getTeacherById);
 
-// const upload = multer({ storage });
-router.post("/setting", upload.single("signature"), createSetting);
-router.post("/account-setting", upload.single("schoolLogo"), createAccount);
+  router.get("/student/:className", getStudentsByClass);
+  router.get("/get-admin", authenticateUser, getAdmin);
+  router.put("/students/:id", authenticateUser, updateStudentById);
+  router.put("/teachers/:id", authenticateUser, updateTeacherById);
 
-router.get("/setting", getSetting);
-router.get("/account-setting", getAccountSetting);
+  router.delete("/users/:userId", deleteUser);
+  router.post("/setting", upload.single("signature"), createSetting);
+  // router.post("/account-setting", upload.single("schoolLogo"), createAccount);
 
-export default router;
+  router.post("/account-setting", multer().single("schoolLogo"), (req, res) => {
+    // Pass the s3 object to the createAccount function
+    createAccount(req, res, s3);
+  });
+
+  router.get("/setting", getSetting);
+  router.get("/account-setting", getAccountSetting);
+
+  return router;
+};
+
+export default commonRoute;
