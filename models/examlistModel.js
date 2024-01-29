46a83f5515e3c0d24ffa70eba2878590395d3b58
OlaniyiHope <olaniyihoppee@gmail.com>
2024-01-29 05:35:29 +0100

    all

--- models/examlistModel.js
+++ models/examlistModel.js
@@ -19,11 +19,11 @@ const ExamlistSchema = new mongoose.Schema(
       required: true,
     },
     fromTime: {
-      type: Date, // Change the type as needed (e.g., String for HH:mm format)
+      type: String, // Change the type as needed (e.g., String for HH:mm format)
       required: true,
     },
     toTime: {
-      type: Date, // Change the type as needed (e.g., String for HH:mm format)
+      type: String, // Change the type as needed (e.g., String for HH:mm format)
       required: true,
     },
     percent: {
