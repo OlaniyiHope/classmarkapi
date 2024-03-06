927fbe5bb326472c265eb900ca9327e6aff950e9
OlaniyiHope <olaniyihoppee@gmail.com>
2024-03-06 12:51:17 +0100

    all

--- routes/examlistRoute.js
+++ routes/examlistRoute.js
@@ -9,16 +9,46 @@ import {
   getExamForStudent,
   getTheoryAnswer,
   getTheoryAnswerByName,
+  editExam,
+  saveStudentTheoryScore,
+  getStudentTheoryScoresByExam,
+  getStudentTheoryScoresByExamAndStudent,
+  calculateTotalScore,
+  getStudentTheoryScoresByStudent,
+  saveTotalScores,
+  getStudentTheoryScoresByStudentAndClassNameAndSubject,
 } from "../controller/examlistController.js";
 import authenticateUser from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Create a new exam (accessible only to admins)
 router.post("/create-exam", authenticateUser, createExam);
+router.post(
+  "/save-student-theory-score",
+
+  saveStudentTheoryScore
+);
+router.post("/save-total-scores", saveTotalScores);
+router.get(
+  "/student-theory-scores/:studentId",
+  getStudentTheoryScoresByStudent
+);
+router.get(
+  "/student-theory-scores/:className/:studentName/:subjectName",
+  getStudentTheoryScoresByStudentAndClassNameAndSubject
+);
+
 router.get("/get-exams-by-class/:classId", authenticateUser, getExamsByClass);
 
 // Get a list of all exams
 router.get("/get-exam", getAllExams);
+router.get("/student-theory-scores/:examId", getStudentTheoryScoresByExam);
+router.get(
+  "/student-theory-scores/:examId/:studentId",
+  getStudentTheoryScoresByExamAndStudent
+);
+
+router.get("/calculate-total-score/:studentId", calculateTotalScore);
 
 // Get a specific exam by ID
 router.get("/get-exam/:id", getExamById);
@@ -38,6 +68,8 @@ router.get(
   "/get-theory-answer-by-name/className/:className/student/:studentName/subject/:subject",
   getTheoryAnswerByName
 );
+// Edit an existing exam (accessible only to admins)
+router.put("/edit-exam/:examId", editExam);
 
 // Update an existing exam by ID (accessible only to admins)
 // router.put("/:id", updateExam);
