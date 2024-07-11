0f3f2494c390d38940446945759acb6ebf9d240e
OlaniyiHope <olaniyihoppee@gmail.com>
2024-07-11 14:08:51 +0100

    all

--- routes/noticeRoute.js
+++ routes/noticeRoute.js
@@ -11,10 +11,10 @@ import {
 } from "../controller/noticeController.js";
 
 const router = express.Router();
-
+router.get("/get-notice/:id", getNoticebyId);
 router.post("/create-notice", createNotice);
 router.get("/get-notice/:role", getNotice);
-router.get("/get-notice/:id", getNoticebyId);
+
 router.get("/get-all-notices", getallNotice);
 router.put("/edit-notice/:id", editNotice);
 router.delete("/delete-notice/:id", deleteNotice);
