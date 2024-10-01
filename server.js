4b5f82515fbd0213b82dfaccb31fb0d9ddc0089d
olaniyihope2 <olaniyihope2@gmail.com>
2024-10-01 18:01:02 +0100

    all

--- server.js
+++ server.js
@@ -221,5 +221,5 @@ app.use("/api/", pastQuestRoute);
 app.use("/api/", practicePqRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
