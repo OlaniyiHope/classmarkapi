45ca2eab617c67b65bceeb6e8e7c9519d3b8f171
olaniyihope2 <olaniyihope2@gmail.com>
2024-10-25 14:49:06 +0100

    All

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ const s3 = new S3({
 });
 
 const corsOptions = {
-  origin: ["http://localhost:3001", "https://hlhs.edupro.com.ng"],
+  origin: ["http://localhost:3000", "https://hlhs.edupro.com.ng"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
