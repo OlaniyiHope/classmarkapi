31449c8b948c2b97f6e2be0a33d3d6f89bc1bf63
LifeMirrorApp <lifemirrorapp@gmail.com>
2025-04-10 13:06:09 +0100

    all

--- routes/subRoute.js
+++ routes/subRoute.js
@@ -13,7 +13,6 @@ import authenticateUser from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// router.post("/create-subject", authenticateUser, createSubject);
 router.post("/create-subject/:sessionId", authenticateUser, createSubject);
 router.post(
   "/addSessionToSubjectWithoutSession",
